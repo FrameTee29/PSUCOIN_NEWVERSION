68fbce1761eaad6f6db9bb913af9348e00f818f1
FrameTee29 <teeraphat123@hotmail.com>
2020-03-08 14:34:56 +0700

    Icon link

--- FrontEnd/pages/_document.js
+++ FrontEnd/pages/_document.js
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Head, Html, NextScript, Main } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -25,5 +25,20 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="icon" href="/static/images/psu-logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
